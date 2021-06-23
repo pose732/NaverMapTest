ffed64e778ccb4de78fc0fce26bda32ddea562ff
sia <pose732@gmail.com>
2021-06-23 12:40:14 +0900

    스피너추가

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private Spinner spinner;
+    private NaverMap naverMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         FragmentManager fm = getSupportFragmentManager();
         MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
         if (mapFragment == null) {
@@ -23,9 +32,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         }
         mapFragment.getMapAsync(this);
+
     }
     @Override
     public void onMapReady(@NonNull @org.jetbrains.annotations.NotNull NaverMap naverMap) {
         naverMap.setMapType(NaverMap.MapType.Hybrid);
+        final ArrayList<String> list = new ArrayList<>();
+        list.add("일반지도");
+        list.add("위성지도");
+        list.add("하이브리드");
+
+        spinner =(Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        naverMap.setMapType(NaverMap.MapType.Basic);
+                        break;
+                    case 1:
+                        naverMap.setMapType(NaverMap.MapType.Satellite);
+                        break;
+                    case 2:
+                        naverMap.setMapType(NaverMap.MapType.Hybrid);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
