3608c6b3bc99989ff495bf2a93a66c8e77ee2cd3
sia <pose732@gmail.com>
2021-06-25 16:11:59 +0900

    3곳 마커찍기

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,23 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private Spinner spinner;
-    private NaverMap naverMap;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         FragmentManager fm = getSupportFragmentManager();
         MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
         if (mapFragment == null) {
@@ -36,12 +42,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
     @Override
     public void onMapReady(@NonNull @org.jetbrains.annotations.NotNull NaverMap naverMap) {
-        naverMap.setMapType(NaverMap.MapType.Hybrid);
+
+        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new LatLng(35.9462109232765, 126.68205850973268));
+        naverMap.moveCamera(cameraUpdate);
+
         final ArrayList<String> list = new ArrayList<>();
-        list.add("일반지도");
         list.add("위성지도");
+        list.add("일반지도");
         list.add("하이브리드");
 
+        checkBox=(CheckBox)findViewById(R.id.checkBox);
         spinner =(Spinner)findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, list);
@@ -52,10 +62,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0:
-                        naverMap.setMapType(NaverMap.MapType.Basic);
+                        naverMap.setMapType(NaverMap.MapType.Satellite);
                         break;
                     case 1:
-                        naverMap.setMapType(NaverMap.MapType.Satellite);
+                        naverMap.setMapType(NaverMap.MapType.Basic);
                         break;
                     case 2:
                         naverMap.setMapType(NaverMap.MapType.Hybrid);
@@ -69,6 +79,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             }
         });
-
+        naverMap.setOnMapClickListener((point, coord) ->
+                Toast.makeText(this, coord.latitude + "  ,   " + coord.longitude,
+                        Toast.LENGTH_SHORT).show());
+        Marker marker1 = new Marker();
+        marker1.setPosition(new LatLng(35.9462109232765, 126.68205850973268));
+        marker1.setMap(naverMap);
+        Marker marker2 = new Marker();
+        marker2.setPosition(new LatLng(35.98785054141381, 126.74903971349413));
+        marker2.setMap(naverMap);
+        Marker marker3 = new Marker();
+        marker3.setPosition(new LatLng(35.970526679440674, 126.61734220993289));
+        marker3.setMap(naverMap);
+        
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    naverMap.setLayerGroupEnabled(NaverMap. LAYER_GROUP_CADASTRAL, true);
+                }
+                else {
+                    naverMap.setLayerGroupEnabled(NaverMap. LAYER_GROUP_CADASTRAL, false);
+                }
+            }
+        });
     }
 }
